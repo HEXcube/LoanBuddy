f2136c754b8b7831959e372e62dbe19786d475b5
HEXcube <hexcubed@gmail.com>
2020-04-30 17:59:29 +0530

    Add script to enable and disable Vuforia Engine

---
+++ Assets/Scripts/VuforiaEnableDisable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+/*
+    Enable and disable Vuforia
+    https://forum.unity.com/threads/vuforia-initialization-and-deinitialization.514794/#post-3956611
+*/
+public class VuforiaEnableDisable : MonoBehaviour
+{
+    public static void EnableVuforia()
+    {
+        VuforiaBehaviour.Instance.enabled = true;
+    }
+
+    public static void DisableVuforia()
+    {
+        VuforiaBehaviour.Instance.enabled = false;
+    }
+}
